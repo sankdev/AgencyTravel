55e4703d015f2c3bfb36fe3682c41fca7a2cd0a4
sank <88152125+sankdev@users.noreply.github.com>
2025-03-22 14:21:02 +0000

    modification e vite config

--- travelApp/vite.config.js
+++ travelApp/vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
   server: {
     port: 3000,
+    hmr: mode === 'development' // Désactive WebSocket en production
   },
-});
+}));
